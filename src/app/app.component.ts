9217b55ec0dd94bfb069c034718e9778e3c086b3
Icarocruzz <5kandradee@gmail.com>
2025-02-14 11:10:21 -0300

    feat: mobile e desktop view ajustados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './components/navbar/navbar.component';
 import {MatIconModule} from '@angular/material/icon';
+import {BannerComponent} from './components/banner/banner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, effect, signal} from '@angular/core';
+import {scroll, scrollInfo} from 'motion';
 
 @Component({
   selector: 'app-navbar',
@@ -7,16 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  menuValue:boolean = false;
-  menu_icon:string = 'fa-solid fa-bars'
+  menuValue: boolean = false;
+  menu_icon: string = 'bi bi-list'
 
-  openMenu () {
+  openMenu() {
     this.menuValue = !this.menuValue;
-    this.menu_icon = this.menuValue ? 'fa-solid fa-x' : 'fa-solid fa-bars';
+    this.menu_icon = this.menuValue ? 'bi bi-x' : 'bi bi-list';
   }
 
-  closeMenu () {
+  closeMenu() {
     this.menuValue = false;
-    this.menu_icon = 'fa-solid fa-bars';
+    this.menu_icon = 'bi bi-list';
   }
+
 }
