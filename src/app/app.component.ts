a6d624f5833ceab73ca5f55277a4d3870d97031b
Icarocruzz <5kandradee@gmail.com>
2025-02-14 12:03:50 -0300

    feat: implementação do component "Objetivos"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './components/navbar/navbar.component';
 import {MatIconModule} from '@angular/material/icon';
 import {BannerComponent} from './components/banner/banner.component';
+import {ObjetivosComponent} from './components/objetivos/objetivos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, BannerComponent],
+  imports: [RouterOutlet, NavbarComponent, BannerComponent, ObjetivosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
